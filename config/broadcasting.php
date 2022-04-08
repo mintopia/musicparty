5d87f99784d7af6c65090cb6720c56dc9a82aaeb
Jessica Smith <jessica.smith@fasthosts.com>
2022-04-08 12:55:17 +0100

    Update default config for pusher to prevent builds failing

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -32,8 +32,8 @@ return [
 
         'pusher' => [
             'driver' => 'pusher',
-            'key' => env('PUSHER_APP_KEY'),
-            'secret' => env('PUSHER_APP_SECRET'),
+            'key' => env('PUSHER_APP_KEY', ''),
+            'secret' => env('PUSHER_APP_SECRET', ''),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
