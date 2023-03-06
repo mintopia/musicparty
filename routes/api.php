10fcba3067dc265dcb62d4809d8b771b298dc096
Jessica Smith <jessica.smith@digicert.com>
2023-03-06 12:18:03 +0000

    Add support for Trebble

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('v1')->name('api.v1.')->group(function() {
+Route::prefix('v1')->name('api.v1.')->middleware('treblle')->group(function() {
     Route::get('ping', function() {
         return response()->noContent();
     })->name('ping');
