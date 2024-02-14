8c8d9d1bcbb5f06d3214b7cb0a48d7de6ca8f6e7
Jessica Smith <jess@mintopia.net>
2024-02-14 10:52:50 +0000

    Make party update jobs unique so we don't get multiple of them

--- app/Jobs/PartyUpdate.php
+++ app/Jobs/PartyUpdate.php
@@ -5,15 +5,25 @@ namespace App\Jobs;
 use App\Models\Party;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 
-class PartyUpdate implements ShouldQueue
+class PartyUpdate implements ShouldQueue, ShouldBeUnique
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
-
+    public int $uniqueFor = 3600;
+ 
+    /**
+     * Get the unique ID for the job.
+     */
+    public function uniqueId(): string
+    {
+        return $this->party->id;
+    }
+    
     /**
      * Create a new job instance.
      */
