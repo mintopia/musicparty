b0cfab4f1c906f85a518f873a424b5eb446db3a8
Jessica Smith <jess@mintopia.net>
2024-03-13 19:44:31 +0000

    Change scopes for search app to what it requires

--- app/Services/SocialProviders/SpotifySearchProvider.php
+++ app/Services/SocialProviders/SpotifySearchProvider.php
@@ -10,4 +10,13 @@ class SpotifySearchProvider extends SpotifyProvider
     protected string $name = 'Spotify Search';
     protected string $code = 'spotifysearch';
     protected bool $supportsAuth = false;
+
+    protected function getSocialiteProvider()
+    {
+        return Socialite::buildProvider(Provider::class, [
+            'client_id' => $this->provider->getSetting('client_id'),
+            'client_secret' => $this->provider->getSetting('client_secret'),
+            'redirect' => $this->redirectUrl,
+        ]);
+    }
 }
