7cc72b6beb5592cd662d071425d3e24152034ded
Jessica Smith <jess@mintopia.net>
2024-06-18 02:05:15 +0100

    Add Laravel Echo and Pusher JS libraries for external websocket integration

--- app/Policies/PartyPolicy.php
+++ app/Policies/PartyPolicy.php
@@ -11,7 +11,7 @@ class PartyPolicy
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user): bool
+    public function viewAny(?User $user): bool
     {
         return true;
     }
@@ -19,7 +19,7 @@ class PartyPolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Party $party): bool
+    public function view(?User $user, Party $party): bool
     {
         return true;
     }
