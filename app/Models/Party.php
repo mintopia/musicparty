8620ae73c8ec75a19c7c21ac84c650ae8007ffd0
Jessica Smith <jess@mintopia.net>
2024-12-28 13:48:09 +0000

    Handle playlist not containing track data

--- app/Models/Party.php
+++ app/Models/Party.php
@@ -572,6 +572,9 @@ class Party extends Model
             ->toArray();
 
         $tracks = array_filter($tracks, function ($track) use ($existingIds) {
+            if ($track->track === null) {
+                return false;
+            }
             return !in_array($track->track->id, $existingIds);
         });
 
